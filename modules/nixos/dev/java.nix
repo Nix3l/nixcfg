add48f2424bb3f28f45aaf4031a5973250d49b97
nix3l <momanianas123@gmail.com>
2025-07-25 21:14:12 +0300

    updated java module to add option for eclipse/jetbrains

--- modules/nixos/dev/java.nix
+++ modules/nixos/dev/java.nix
@@ -4,10 +4,14 @@
     options.mods.dev.java = with lib; {
         enable = mkEnableOption "java devtools";
         jetbrains.enable = mkEnableOption "jetbrains idea community";
+        eclipse.enable = mkEnableOption "eclipse";
     };
 
     config = lib.mkIf config.mods.dev.java.enable {
         programs.java.enable = true;
-        environment.systemPackages = with pkgs; [ eclipses.eclipse-java ];
+        environment.systemPackages = with pkgs; [
+            (lib.optional config.mods.dev.java.eclipse.enable eclipses.eclipse-java)
+            (lib.optional config.mods.dev.java.jetbrains.enable jetbrains.idea-community)
+        ];
     };
 }
