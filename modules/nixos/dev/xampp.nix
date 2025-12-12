ecae30659dbd97536c9639a8fbf8d64f2943e65d
ae92 <nidi.6002@gmail.com>
2025-12-13 00:03:43 +0300

    dev: fucking around with the xampp module

---
+++ modules/nixos/dev/xampp.nix
@@ -0,0 +1,50 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.mods.dev.xampp;
+in
+{
+  options.mods.dev.xampp = with lib; {
+    enable = mkEnableOption "xampp";
+
+    documentRoot = mkOption {
+      type = types.str;
+      default = "/var/www/xampp";
+    };
+
+    hostName = mkOption {
+      type = types.str;
+      default = "localhost";
+    };
+
+    withMysql = mkOption {
+      type = types.bool;
+      default = true;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    mods.dev.mysql.enable = cfg.withMysql;
+
+    services = {
+      httpd = {
+        enable = true;
+        enablePHP = true;
+
+        virtualHosts.${cfg.hostName} = {
+          documentRoot = cfg.documentRoot;
+        };
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 80 ];
+
+    systemd.tmpfiles.rules = [
+      "d ${cfg.documentRoot} 0755 - - -"
+      "f ${cfg.documentRoot}/index.php 0644 - - - - <?php echo 'XAMPP-like stack is running ✅<br>'; phpinfo(); ?>"
+    ];
+
+    environment.systemPackages = with pkgs; [ php phpPackages.composer ];
+  };
+}
+
