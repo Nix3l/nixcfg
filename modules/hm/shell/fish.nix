4d2994ace313d9fe5e8cea491e676863c390ca8b
nix3l <momanianas123@gmail.com>
2026-04-09 00:42:30 +0300

    fish: change prompt + gruvbox

--- modules/hm/shell/fish.nix
+++ modules/hm/shell/fish.nix
@@ -1,4 +1,4 @@
-{ lib, config, osConfig, ... }:
+{ lib, config, osConfig, pkgs, ... }:
 
 let
     cfg = config.hm.mods.fish;
@@ -11,6 +11,26 @@ in
     config = lib.mkIf cfg.enable {
         programs.fish = {
             enable = true;
+
+            plugins = [
+                {
+                    name = "gruvbox";
+                    src = pkgs.fetchFromGitHub {
+                        owner = "Jomik";
+                        repo = "fish-gruvbox";
+                        rev = "80a6f3a7b31beb6f087b0c56cbf3470204759d1c";
+                        sha256 = "sha256-vL2/Nm9Z9cdaptx8sJqbX5AnRtfd68x4ZKWdQk5Cngo=";
+                    };
+                }
+            ];
+
+            interactiveShellInit = ''
+                function fish_greeting
+                    # todo
+                end
+
+                theme_gruvbox dark
+            '';
 
             preferAbbrs = true;
             shellAbbrs = {
